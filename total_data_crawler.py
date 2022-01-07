1b2ddc9e484be7d0a7712b4f64c9ddb149811ff5
Grand ou <B08310054@ntu.edu.tw>
2022-01-07 12:00:39 +0800

    update crawler

--- total_data_crawler.py
+++ total_data_crawler.py
@@ -1,6 +1,10 @@
-### 匯入套件
+                  ### 匯入套件
 import requests
 from bs4 import BeautifulSoup
+import time
+import os
+import datetime
+import csv
 
 ### 請求網頁並文字解析
 res = requests.get("http://uba.tw/110/Schedule/Details?scheduleId=61E4C940-F182-4912-9241-7599A148EC52")
@@ -12,21 +16,77 @@ html = BeautifulSoup(res.text, "html.parser")
 ### 找尋 tag 為 tr 且 class 為 total_tr
 tr = html.find_all("tr", {"class" : "total_tr"})
 
+
+
+team_name_div = html.find_all('div', {"class" : 'record_tableTitle'})
+date = html.find('div',  {'class':"final_infoName"}).get_text()
+
+team_name1 = team_name_div[0].find('span').get_text()
+team_name2 = team_name_div[1].find('span').get_text()
+# print(team_name_div)
+date = date[:11] + date[15:17]
+print(date)
+date = time.strptime(date, "%Y年%m月%d日%H")
+# print(date)
 ### 建立字典與參數
-total = {"NCCU" : {},
-         "NTU" : {}}
-teams = ["NCCU", "NTU"]
-terms = ["二分", "%", "三分", "%", "罰球", "%", "進攻", "防守", "總計", "助攻", "抄截", "阻攻", "失誤", "犯規", "得分"]
+column = ['日期', '隊伍', "二分", "%", "三分", "%", "罰球", "%", "進攻", "防守", "總計", "助攻", "抄截", "阻攻", "失誤", "犯規", "得分", '勝負']
+total = {team_name1 : [],
+         team_name2 : []}
+teams = [team_name1, team_name2]
+
+
+
+
+
 
 ### 比賽中有兩支隊伍需要爬取
 for i in range(2):
-    team = total[teams[i]] # 指定隊伍
+ 
+    team = [0 for k in range(len(column))] # 指定隊伍
     temp = tr[i]
     count = 0
-    term = 0
+    term = 2
+    team[0] = time.strftime('%Y-%m-%d-%H',date) + '-' + str(i)
+    team[1] = teams[i]
     for j in temp:
         if count >= 4 and j != "\n" and j.text != "":
-            team[terms[term]] = j.text.strip() # 將比賽資料依照順序放入字典
+            team[term] = j.text.strip() # 將比賽資料依照順序放入字典
             term += 1
         else:
             count += 1
+    total[teams[i]] = team
+if int(total[team_name2][-2]) > int(total[team_name1][-2]):
+    # print( total[team_name1][-2])
+    total[team_name2][-1] = 1
+    total[team_name1][-1] = 0
+else:
+    total[team_name2][-1] = 0
+    total[team_name1][-1] = 1
+print(total[team_name2])
+data = [total[team_name1],total[team_name2]]
+'''Append new data after old output as csv, create a new output if old output not found'''
+header = column
+parsed_csv = data
+path = ''
+file_name = 'game_record'
+with open(os.path.join(path, file_name + "_output.csv"), mode='a+', newline='', encoding='UTF-8') as f:
+    line = list(csv.reader(f))
+
+with open(os.path.join(path, file_name + "_output.csv"), mode='r', newline='',  encoding='UTF-8') as f:
+    line = list(csv.reader(f))
+
+with open(os.path.join(path, file_name + "_output.csv"), mode='a+', newline='', encoding='UTF-8_sig') as csvfile:
+    writer = csv.writer(csvfile)
+    if len(line) == 0:
+        print("empty")
+        writer.writerow(header)
+        writer.writerows(parsed_csv)
+    else:
+        for i in range(0, len(parsed_csv)):
+            last_date = datetime.datetime.strptime(
+                line[len(line)-1][0], "%Y-%m-%d-%H-%M")
+            current_date = datetime.datetime.strptime(
+                parsed_csv[i][0], "%Y-%m-%d-%H-%M")
+            if current_date > last_date:
+                print('newdata')
+                writer.writerow(parsed_csv[i])
