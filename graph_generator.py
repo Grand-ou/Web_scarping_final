d8f0b73d076ecbac74c1f6a26c390d9846fdbbc4
Chun-Lin Yu <damien.cl.yu@gmail.com>
2022-01-09 15:59:21 +0800

    Generating graphs

--- graph_generator.py
+++ graph_generator.py
@@ -9,7 +9,7 @@ df2 = df1["三分"].str.split("-", expand = True)
 df1["命中"] = df2[0]
 df1["出手"] = df2[1]
 
-x = pd.to_datetime(df1["日期"].str[:10]) # 日期
+x = df1["日期"] # 日期
 y1 = df1["命中"] # 命中
 y2 = df1["出手"] # 出手
 y3 = df1["%.1"] / 100 # 命中率
@@ -19,28 +19,28 @@ myfont = FontProperties(fname = r"./NotoSansTC-Black.otf")
 plt.plot(x, y1, "-")
 plt.plot(x, y2, "-")
 # 設定標頭和字體
-plt.title('隨著賽事進展出手數和命中數的趨勢', fontproperties=myfont, fontsize = 12)
+plt.title('三分隨著賽事進展出手數和命中數的趨勢', fontproperties=myfont, fontsize = 12)
 # 設定 x 軸標頭和字體
 plt.xlabel('日期', fontproperties=myfont, fontsize = 12)
 # 設定 y 軸標頭和字體
 plt.ylabel('數量', fontproperties=myfont, fontsize = 12)
 # 設定右上角說明圖示字體
 plt.legend(["命中數", "出手數"], prop = myfont, fontsize = 12)
-plt.xticks(rotation=-15)
+plt.xticks(rotation=90)
 plt.yticks(range(0, 50, 5))
 plt.savefig('三分_命中數與出手數_by_date')
 plt.clf()
 
 plt.plot(x, y3, "-")
 # 設定標頭和字體
-plt.title('隨著賽事進展命中率的趨勢', fontproperties=myfont, fontsize = 12)
+plt.title('三分隨著賽事進展命中率的趨勢', fontproperties=myfont, fontsize = 12)
 # 設定 x 軸標頭和字體
 plt.xlabel('日期', fontproperties=myfont, fontsize = 12)
 # 設定 y 軸標頭和字體
 plt.ylabel('命中率', fontproperties=myfont, fontsize = 12)
 # 設定右上角說明圖示字體
 plt.legend(["命中率"], prop = myfont, fontsize = 12)
-plt.xticks(rotation=-15)
+plt.xticks(rotation=90)
 plt.savefig('三分_命中率_by_date')
 plt.clf()
 
@@ -50,7 +50,7 @@ df2 = df1["二分"].str.split("-", expand = True)
 df1["命中"] = df2[0]
 df1["出手"] = df2[1]
 
-x = pd.to_datetime(df1["日期"].str[:10]) # 日期
+x = df1["日期"] # 日期
 y1 = df1["命中"] # 命中
 y2 = df1["出手"] # 出手
 y3 = df1["%"] / 100 # 命中率
@@ -60,27 +60,27 @@ myfont = FontProperties(fname = r"./NotoSansTC-Black.otf")
 plt.plot(x, y1, "-")
 plt.plot(x, y2, "-")
 # 設定標頭和字體
-plt.title('隨著賽事進展出手數和命中數的趨勢', fontproperties=myfont, fontsize = 12)
+plt.title('二分隨著賽事進展出手數和命中數的趨勢', fontproperties=myfont, fontsize = 12)
 # 設定 x 軸標頭和字體
 plt.xlabel('日期', fontproperties=myfont, fontsize = 12)
 # 設定 y 軸標頭和字體
 plt.ylabel('數量', fontproperties=myfont, fontsize = 12)
 # 設定右上角說明圖示字體
 plt.legend(["命中數", "出手數"], prop = myfont, fontsize = 12)
-plt.xticks(rotation=-15)
+plt.xticks(rotation=90)
 plt.yticks(range(0, 45, 5))
 plt.savefig('二分_命中數與出手數_by_date')
 plt.clf()
 
 plt.plot(x, y3, "-")
 # 設定標頭和字體
-plt.title('隨著賽事進展命中率的趨勢', fontproperties=myfont, fontsize = 12)
+plt.title('二分隨著賽事進展命中率的趨勢', fontproperties=myfont, fontsize = 12)
 # 設定 x 軸標頭和字體
 plt.xlabel('日期', fontproperties=myfont, fontsize = 12)
 # 設定 y 軸標頭和字體
 plt.ylabel('命中率', fontproperties=myfont, fontsize = 12)
 # 設定右上角說明圖示字體
 plt.legend(["命中率"], prop = myfont, fontsize = 12)
-plt.xticks(rotation=-15)
+plt.xticks(rotation=90)
 plt.savefig('二分_命中率_by_date')
 plt.clf()
